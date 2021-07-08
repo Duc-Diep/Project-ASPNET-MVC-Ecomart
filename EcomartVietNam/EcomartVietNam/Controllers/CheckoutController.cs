e6db0c26929bb2eb4b2a1645a7abcbfc00f358ec
Hoàng Duy Khánh <hdkhanhit@gmai.com>
2021-07-08 21:11:48 +0700

    add template

---
+++ EcomartVietNam/EcomartVietNam/Controllers/CheckoutController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EcomartVietNam.Controllers
+{
+    public class CheckoutController : Controller
+    {
+        // GET: Checkout
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
