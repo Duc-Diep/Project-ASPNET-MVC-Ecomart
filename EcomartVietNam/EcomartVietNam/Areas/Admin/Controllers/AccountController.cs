2e6fbdef01fa47f0a7975f4841f1b9b786645991
Đức Điệp <=>
2021-07-11 10:15:03 +0700

    add entity

---
+++ EcomartVietNam/EcomartVietNam/Areas/Admin/Controllers/AccountController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EcomartVietNam.Areas.Admin.Controllers
+{
+    public class AccountController : Controller
+    {
+        // GET: Admin/Account
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Create()
+        {
+            return View();
+        }
+        public ActionResult Update()
+        {
+            return View();
+        }
+    }
+}
