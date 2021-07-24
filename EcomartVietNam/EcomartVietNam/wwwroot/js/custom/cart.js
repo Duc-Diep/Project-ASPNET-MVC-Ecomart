bb71e4638adfe1a1cdf0fc68bb1da9120b57cde1
Hoàng Duy Khánh <hdkhanhit@gmai.com>
2021-07-24 15:20:09 +0700

    add cart

---
+++ EcomartVietNam/EcomartVietNam/wwwroot/js/custom/cart.js
@@ -0,0 +1,45 @@
+﻿
+const addToCart = async (id) => {
+        const response = await fetch(`/Product/Index/${id}`);
+        const data = await response.json();
+        if (data !== "NOT_FOUND") {
+            let productIds = localStorage.getItem("productIds");
+            if (!productIds) {
+                productIds = `${id}`;
+            } else {
+                productIds += `,${id}`;
+            }
+
+            localStorage.setItem("productIds", productIds);
+            fetchCart(data, 'plus');
+        }
+}
+
+const fetchCart = async (data, type) => {
+    const productIds = localStorage.getItem("productIds");
+    let amount = localStorage.getItem("amount");
+    if (data) {
+        if (type === 'plus') {
+            amount = Number(amount) + Number(data.price);
+            localStorage.setItem("amount", amount);
+        } else {
+            amount = Number(amount) - Number(data.price);
+            localStorage.setItem("amount", amount);
+        }
+
+    }
+
+    if (amount) {
+        document.getElementById("total-amount").innerText = `${localStorage.getItem("amount")} vnđ`;
+    }
+
+    const ids = productIds?.split(",");
+    const response = await fetch(`/Product/List?ids=${ids?.toString()}`);
+    const products = await response.json();
+    if (products !== "NOT_FOUND") {
+        document.getElementById("hdk-count").innerText = products.length;
+    }
+}
+
+fetchCart();
+
